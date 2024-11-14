b14ff195a8ecf7097c1a9469137ad0978d6721a8
Juraj <juraj.cizic@fer.unizg.hr>
2024-11-14 14:13:35 +0100

    Dodan API za prognozu

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const authController = require('../controllers/authController');
+const axios = require('axios');
 const router = express.Router();
 
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'], 
@@ -20,10 +21,35 @@ router.get('/login', authController.verifyOrRefreshAccessToken, (req, res) => {
   }
 });
 
-router.get('/pocetna', authController.verifyOrRefreshAccessToken, (req, res) => {
+router.get('/pocetna', authController.verifyOrRefreshAccessToken, async (req, res) => {
   if (req.user) {
     const user = req.user;
-    res.status(200).json(user);
+
+    //prognoza
+    const city = 'Zagreb';
+    try {
+      const weatherResponse = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+        params: {
+          q: city,
+          appid: process.env.WEATHER_API_KEY,
+          units: 'metric'
+        }
+      });
+      const weatherData = {
+        temperature: weatherResponse.data.main.temp,
+        description: weatherResponse.data.weather[0].description,
+        icon: weatherResponse.data.weather[0].icon,
+        city: weatherResponse.data.name,
+        country: weatherResponse.data.sys.country
+      };
+
+      console.log(user, weatherData)
+      res.status(200).json({ user, weather: weatherData });
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+
+    res.status(200).json({user, weather: null});
   } else {
     res.status(401).json({ message: 'Unauthorized' });
   }
