c577a1f89c1f9f733010264f5bbd9a36514b6d7a
Juraj Čižić <juraj.cizic@fer.unizg.hr>
2024-11-16 02:16:33 +0100

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -18,6 +18,7 @@ const logout = (req, res) => {
 
 const verifyOrRefreshAccessToken = async (req, res, next) => {
   console.log('Middleware reached!');
+  console.log('Request Headers:', req.headers);
   console.log('Verify req:', req.data);
   console.log('Verify res:', res.data);
   console.log('Cookies in request:', req.cookies);
