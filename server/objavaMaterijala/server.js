290129c7ebcf881466566b91a609d1143ab5ad16
Damjan Popović <damjan.popovic@fer.unizg.hr>
2025-01-12 17:28:27 +0100

    Sinkronizacija Google Drive repozitorija sa SQL-om (fali dohvat autora)

--- server/objavaMaterijala/server.js
+++ server/objavaMaterijala/server.js
@@ -21,7 +21,33 @@ const drive = google.drive({ version: "v3", auth: driveAuth });
 
 const GOOGLE_DRIVE_FOLDER_ID = "1I9H0ooP32aYfxf30jwJscSvHoMGa70FK";
 
+//const ConfirmedUser = require('../auth/models/ConfirmedUser');
+//const User = require('../models/User');
+const client = require('../../database/connection.js');
+client.connect();
+
+/* 
+    //glavni repozitorij, treba runnat jednom prije korištenja (na Renderu će bit automatski)
+    const insertQueryRep = `insert into REPOZITORIJ(repID, imeRep, školaID) 
+    values ($1, $2, $3)`;
+    const valuesRep = [GOOGLE_DRIVE_FOLDER_ID, 'Noodle', 1];
+
+    await client.query(insertQueryRep, valuesRep);
+*/
+
 app.use(express.static(path.join(__dirname, "public")));
+
+function formatFileSize(bytes) {
+  if (bytes < 1024) {
+    return `${bytes} B`;
+  } else if (bytes < 1024 * 1024) {
+    return `${(bytes / 1024).toFixed(2)} KB`;
+  } else if (bytes < 1024 * 1024 * 1024) {
+    return `${(bytes / (1024 * 1024)).toFixed(2)} MB`;
+  } else {
+    return `${(bytes / (1024 * 1024 * 1024)).toFixed(2)} GB`;
+  }
+}
 
 app.post("/upload", upload.single("file"), async (req, res) => {
   const filePath = path.join(__dirname, req.file.path);
@@ -36,7 +62,22 @@ app.post("/upload", upload.single("file"), async (req, res) => {
         mimeType: req.file.mimetype,
         body: fs.createReadStream(filePath),
       },
+      fields: 'id, size',
     });
+
+    //const user = await ConfirmedUser.findById(req.body._id);
+    const insertQueryLink = `insert into LINK(brojPregleda, autor, datumObjave, linkTekst, repID) 
+    values ($1, $2, date_trunc('second', CURRENT_TIMESTAMP), $3, $4)`;
+    const insertQueryDatoteka = `insert into DATOTEKA(veličina, linkTekst) values ($1, $2)`;
+    const fileLink = 'https://drive.google.com/file/d/'+response.data.id+'/view';
+
+    const valuesLink = ['0', 'jurica cizic'/*user.name + " " + user.surname*/, 
+      fileLink, GOOGLE_DRIVE_FOLDER_ID];
+    const fileSize = formatFileSize(response.data.size);
+    const valuesDatoteka = [fileSize, fileLink];
+    
+    await client.query(insertQueryLink, valuesLink);
+    await client.query(insertQueryDatoteka, valuesDatoteka);
 
     fs.unlinkSync(filePath);
 
@@ -65,6 +106,7 @@ app.get("/download/:id", async (req, res) => {
   const fileId = req.params.id;
 
   try {
+    const fileUpdateDownloads = await drive.files.get({ fileId });
     const fileMetadata = await drive.files.get({
       fileId,
       fields: "name, mimeType",
@@ -77,6 +119,11 @@ app.get("/download/:id", async (req, res) => {
       { fileId, alt: "media" },
       { responseType: "stream" }
     );
+
+    const updateDownloads = `update LINK set brojPregleda = (CAST(brojPregleda AS INT) + 1)::VARCHAR where linkTekst = $1`;
+    const updateDownloadsValue = ['https://drive.google.com/file/d/'+fileUpdateDownloads.data.id+'/view'];
+
+    await client.query(updateDownloads, updateDownloadsValue);
 
     res.setHeader("Content-Disposition", `attachment; filename="${fileName}"`);
     res.setHeader("Content-Type", mimeType);
@@ -95,7 +142,17 @@ app.delete("/delete/:id", async (req, res) => {
   const fileId = req.params.id;
 
   try {
+    const deleteFile = await drive.files.get({ fileId });
+    
+    const deleteDatoteka = `delete from DATOTEKA where linkTekst = $1`;
+    const deleteLink = `delete from LINK where linkTekst = $1`;
+    const valuesDeleteDatotekaAndLink = ['https://drive.google.com/file/d/'+deleteFile.data.id+'/view'];
+
+    await client.query(deleteDatoteka, valuesDeleteDatotekaAndLink);
+    await client.query(deleteLink, valuesDeleteDatotekaAndLink);
+
     await drive.files.delete({ fileId });
+
     res.status(200).send("File deleted successfully");
   } catch (error) {
     console.error("Error deleting file:", error.message);
