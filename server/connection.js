695e869403b48249d503f4c863efcd226ba710d6
Juraj Čižić <juraj.cizic@fer.unizg.hr>
2025-01-14 22:43:42 +0100

    Update connection.js

--- server/connection.js
+++ server/connection.js
@@ -5,7 +5,10 @@ const client = new Client({
     user: process.env.DB_USER,
     port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = client;
