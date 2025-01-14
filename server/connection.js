0abd5b6e968aea328708245a158095f34f9dd09e
Juraj Čižić <juraj.cizic@fer.unizg.hr>
2025-01-14 22:28:56 +0100

    Create connection.js

---
+++ server/connection.js
@@ -0,0 +1,11 @@
+const { Client } = require('pg');
+
+const client = new Client({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+module.exports = client;
